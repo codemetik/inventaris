e0455a1fcb50c65451432187efba83e90cb31d38
codemetik <irfanovaiyalkharish03@gmail.com>
2025-02-14 13:44:22 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -4,15 +4,15 @@
 // $pass = "smksapra2123";
 // $db = "smkg6671_wadompet";
 
-// $host = "localhost"; 
-// $username = "smkg6671_paktaris"; 
-// $password = "paktaris"; 
-// $database = "smkg6671_db_paktaris"; 
+$host = "localhost"; 
+$username = "smkg6671_paktaris"; 
+$password = "paktaris"; 
+$database = "smkg6671_db_paktaris"; 
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "db_inventaris";
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db = "db_inventaris";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db) 
 or die('Could not connect : ' . mysqli_connect_error());
