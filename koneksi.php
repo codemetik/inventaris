6e8290885b2282d4dafb283714de989f2fb1b774
codemetik <irfanovaiyalkharish03@gmail.com>
2025-03-07 08:37:40 +0700

    memastikan kembali

--- koneksi.php
+++ koneksi.php
@@ -4,15 +4,15 @@
 // $pass = "smksapra2123";
 // $db = "smkg6671_wadompet";
 
-// $host = "localhost"; 
-// $user = "smkg6671_paktaris"; 
-// $pass = "paktaris"; 
-// $db = "smkg6671_db_paktaris"; 
+$host = "localhost"; 
+$user = "smkg6671_paktaris"; 
+$pass = "paktaris"; 
+$db = "smkg6671_db_paktaris"; 
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "db_paktaris";
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db = "db_paktaris";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db) 
 or die('Could not connect : ' . mysqli_connect_error());
